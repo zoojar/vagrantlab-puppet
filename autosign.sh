e8dde48b21be2b685a51262de6cd609fffec09e2
zoojar <davidnewton@zoojar.com>
2015-09-01 13:54:27 +0100

    added autosign script for csr attributes and global psk

--- autosign.sh
+++ autosign.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#! /bin/bash
+csr=$(< /dev/stdin)
 certname=$1
-whitelist='lab.local'
 
-if [[ $certname == *"$whitelist"* ]]
-then
-  exit 0;
-else
-  exit 1;
+# Get the certificate extension with OID $1 from the csr
+function extension {
+  echo "$csr" | openssl req -noout -text | fgrep -A1 "$1" | tail -n 1 \
+      | sed -e 's/^ *//;s/ *$//'
+}
+
+psk=$(extension '1.3.6.1.4.1.34380.1.1.4')
+
+if grep -q "$psk" /etc/puppetlabs/puppet/global-psk; then
+  echo "Info: Autosigning $certname with global-psk $psk..."
+  exit 0
+else 
+  echo "Warn: Not Autosigning $certname with global-psk $psk - no match."
+  exit 1
 fi
