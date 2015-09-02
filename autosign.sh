5b7f1e26aeb7500480362dd97f13378e75022083
zoojar <davidnewton@zoojar.com>
2015-09-02 08:41:52 +0100

    added custom attributes with a global psk to demonstrate the use of secure policy-based autosigning

--- autosign.sh
+++ autosign.sh
@@ -2,18 +2,12 @@
 csr=$(< /dev/stdin)
 certname=$1
 
-# Get the certificate extension with OID $1 from the csr
-function extension {
-  echo "$csr" | openssl req -noout -text | fgrep -A1 "$1" | tail -n 1 \
-      | sed -e 's/^ *//;s/ *$//'
-}
-
-psk=$(extension '1.3.6.1.4.1.34380.1.1.4')
+psk=$(echo "$csr" | openssl req -noout -text | fgrep -A1 "1.2.840.113549.1.9.7" | tail -n 1 | sed -e 's/^ *//;s/ *$//')
 
 if grep -q "$psk" /etc/puppetlabs/puppet/global-psk; then
   echo "Info: Autosigning $certname with global-psk $psk..."
   exit 0
-else 
+else
   echo "Warn: Not Autosigning $certname with global-psk $psk - no match."
   exit 1
 fi
